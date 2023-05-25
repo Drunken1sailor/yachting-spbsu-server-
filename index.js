ee0b973b11f1ceeb37e75a9656245b220570d78a
alek_seevil <alexeevilya.ykt2014@mail.ru>
2023-05-25 11:27:22 +0300

    u

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.json());
 app.use(cors({
-	origin: ["http://localhost:3000"],
+	origin: ["http://yachting-spbsu.ru"],
 	methods: ["GET", "POST"],
 	credentials: true
 }));
