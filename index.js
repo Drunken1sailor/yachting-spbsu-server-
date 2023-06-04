5a0d21f39727cbc15dbe7a7746d2bde43f4fcdec
alek_seevil <alexeevilya.ykt2014@mail.ru>
2023-06-04 16:51:44 +0300

    update origin of cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.json());
 app.use(cors({
-	origin: ["http://localhost:3000"],
+	origin: ["http://localhost:3000","https://yachting-spbsu.ru", "http://yachting-spbsu.ru"],
 	methods: ["GET", "POST"],
 	credentials: true
 }));
